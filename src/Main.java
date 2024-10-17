923a35dcfd19602695bbb03b3f960a310fa0e8f1
Janosch Walter <janowalter@tsn.at>
2024-10-17 22:32:08 +0200

    Alles in der Main asprobiert

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Angebot angebot = new Rabatt(200.0, LocalDate.of(2024,2,14),"3323","Flug nach Dubai");
+        angebot.anzeigen();
     }
 }
