2ba3b8a2b3fbd405be11993cbdb9080e5430ad68
Janosch Walter <janowalter@tsn.at>
2024-10-17 15:45:34 +0200

    Sinnlose Klassen deletet

--- src/MidiDiscount.java
+++
@@ -1,7 +0,0 @@
-import java.time.LocalDate;
-
-public class MidiDiscount extends Rabattstrategie{
-    public MidiDiscount(double regulärpreis, LocalDate flugdatum, String flugnummer, String bezeichnung) {
-        super(regulärpreis, flugdatum, flugnummer, bezeichnung);
-    }
-}
--- src/ZeroDiscount.java
+++
@@ -1,7 +0,0 @@
-import java.time.LocalDate;
-
-public class ZeroDiscount extends Rabattstrategie{
-    public ZeroDiscount(double regulärpreis, LocalDate flugdatum, String flugnummer, String bezeichnung) {
-        super(regulärpreis, flugdatum, flugnummer, bezeichnung);
-    }
-}
